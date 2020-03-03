682e1c33b4a01ff8bcac72276d69f5bf0519cfd7
Roger-mrj <645106533@qq.com>
2020-03-03 14:13:36 +0800

    roger

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,22 @@
   <div id="app">
     <router-view/>
     <tab-bar>
-      <tab-bar-item>
+      <tab-bar-item path="/home">
         <img slot="item-icon" src="./assets/img/tabbar/home.svg" alt="">
         <img slot="item-active-icon" src="./assets/img/tabbar/home-active.svg" alt="">
         <div slot="item-text">首页</div>
       </tab-bar-item>
-      <tab-bar-item>
+      <tab-bar-item path="/category">
         <img slot="item-icon" src="./assets/img/tabbar/category.svg" alt="">
         <img slot="item-active-icon" src="./assets/img/tabbar/category-active.svg" alt="">
         <div slot="item-text">分类</div>
       </tab-bar-item>
-      <tab-bar-item>
+      <tab-bar-item path="/shopcart">
         <img slot="item-icon" src="./assets/img/tabbar/shopcart.svg" alt="">
         <img slot="item-active-icon" src="./assets/img/tabbar/shopcart-active.svg" alt="">
         <div slot="item-text">购物车</div>
       </tab-bar-item>
-      <tab-bar-item>
+      <tab-bar-item path="profile">
         <img slot="item-icon" src="./assets/img/tabbar/profile.svg" alt="">
         <img slot="item-active-icon" src="./assets/img/tabbar/profile-active.svg" alt="">
         <div slot="item-text">我的</div>
--- src/components/tabbar/TabBarItem.vue
+++ src/components/tabbar/TabBarItem.vue
@@ -1,17 +1,36 @@
 <template>
-  <div class="tab-bar-item">
-    <slot v-if="!isActive" name="item-icon"></slot>
-    <slot v-else name="item-active-icon"></slot>
-    <slot name="item-text"></slot>
+  <div class="tab-bar-item" @click="ItemClick">
+    <div v-if="!isActive">
+      <slot name="item-icon"></slot>
+    </div>
+    <div v-else>
+      <slot name="item-active-icon"></slot>
+    </div>
+    <div :class="{active:isActive}">
+      <slot name="item-text"></slot>
+    </div>
   </div>
 </template>
 
 <script>
   export default {
     name: "TabBarItem",
-    data(){
-      return{
-        isActive:true
+    data() {
+      return {
+        // isActive:true,
+      }
+    },
+    props: {
+      path: String,
+    },
+    computed: {
+      isActive() {
+        return this.$route.path.indexOf(this.path) != -1
+      }
+    },
+    methods: {
+      ItemClick() {
+        return this.$router.replace(this.path)
       }
     }
   }
@@ -27,5 +46,10 @@
 
   .tab-bar-item img {
     height: 24px;
+  }
+
+  .active {
+    color: #409EFF;
+    /*background-color: red;*/
   }
 </style>
